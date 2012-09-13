920c98aa91066f722018faf3a29898a7b51ab284
tedliang <teddyliang@gmail.com>
2012-09-13 10:56:52 +1000

    add single class version

---
+++ src/dockfx/DockFX.scala
@@ -0,0 +1,59 @@
+package dockfx
+
+import javafx.stage.StageStyle
+import scalafx.Includes._
+import scalafx.animation.Tweenable.tweenable2KeyFrame
+import scalafx.animation.Timeline
+import scalafx.application.JFXApp
+import scalafx.event.ActionEvent
+import scalafx.scene.Group.sfxGroup2jfx
+import scalafx.scene.effect.Reflection
+import scalafx.scene.image.Image.sfxImage2jfx
+import scalafx.scene.image.Image
+import scalafx.scene.image.ImageView.sfxImageView2jfx
+import scalafx.scene.image.ImageView
+import scalafx.scene.input.MouseEvent.sfxMouseEvent2jfx
+import scalafx.scene.input.MouseEvent
+import scalafx.scene.layout.HBox.sfxHBox2jfx
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color.TRANSPARENT
+import scalafx.scene.paint.Color.sfxColor2jfx
+import scalafx.scene.Group
+import scalafx.scene.Scene
+import scalafx.stage.Stage
+
+object DockFX extends JFXApp {
+
+  stage = new Stage() {
+    scene = new Scene(new Group(
+	    new ImageView {
+	      translateX = 100; translateY = 200
+	      image = new Image("images/dock1.png")
+	      onMouseClicked = (e: MouseEvent) =>
+	        image = new Image("images/dock%s.png" format
+	          (if (e isPrimaryButtonDown) "1" else "2"))
+	    },
+	    new HBox(20) {
+	      translateX = 150; translateY = 220
+	      content = for (name <- Seq("Browser", "Internet", "Mail", "User"))
+	        yield new ImageView {
+	    	  image = new Image("icons/" + name + "-32.png")
+			  effect = new Reflection
+			  onMouseEntered = bouncer play
+			  val bouncer = new Timeline {
+			    keyFrames = Seq(
+			      at (  0 ms) {Set(scaleX -> 1.2, scaleY -> 1.0, translateY ->   0.0)},
+			      at (100 ms) {Set(scaleX -> 1.0, scaleY -> 1.2, translateY ->   0.0)},
+			      at (300 ms) {Set(scaleX -> 1.0, scaleY -> 1.0, translateY -> -20.0)},
+			      at (500 ms) {Set(scaleX -> 1.0, scaleY -> 1.2, translateY ->   0.0)},
+			      at (600 ms) {Set(scaleX -> 1.2, scaleY -> 1.0, translateY ->   0.0)}
+			    )
+			    onFinished = (_: ActionEvent) => if(hover()) play else scaleX = 1.0
+			  }
+	      	}
+	    }
+    ), 550, 300) {fill = TRANSPARENT}
+    initStyle(StageStyle.TRANSPARENT)
+  }
+
+}
